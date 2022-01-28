ffac3e46c2815c88ada397a20d2d50c6b0897279
JaviGM <javi46018@gmail.com>
2022-01-28 21:01:30 +0100

    animacion counter

--- src/components/CounterEffect.tsx
+++ src/components/CounterEffect.tsx
@@ -1,10 +1,13 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { gsap } from 'gsap';
 
 const MAX_COUNT = 10;
 
 export const CounterEffect = () => {
 
-    const [count, setCount] = useState(5);
+    const [count, setCount] = useState(9);
+
+    const counterElement = useRef<HTMLHeadingElement>(null);
     
     const plus = () => {
         setCount(prev => Math.min(prev + 1, MAX_COUNT));
@@ -12,14 +15,27 @@ export const CounterEffect = () => {
 
     //TODO no deveria pasar de 10
     useEffect(() => {
-    
-    }, []);
+        if (count < 10) return;
+
+        console.log('count', count);
+
+        const timeline = gsap.timeline();
+
+        timeline.to(
+            counterElement.current,
+            {y: -10, duration: 0.2, ease: 'ease.out'}
+        ).to(
+            counterElement.current,
+            {y: 0, duration: 1, ease: 'bounce.out'}
+        );
+
+    }, [count]);
 
     return (
         <>
             <h1> CounterEffect </h1>
 
-            <span> {count} </span>
+            <h2 ref={counterElement}> {count} </h2>
 
             <button onClick={ plus }>
                 +1
